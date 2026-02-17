dd9c3db7117229d1e37a20c68b9b68a3907ccd9c
RasmusEriksson <rasmus.eriksson@elev.ga.ntig.se>
2026-02-17 09:45:16 +0100

    starting with databases

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 import express from "express"
+import pool from "../config/db.js"
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.render("index.njk",
-        { title: "Node js startrepo", message: "Använd det här repot som en grund för dina projekt." }
-    )
+router.get("/", async (req, res, next) => {
+    try {
+        const [rows] = await pool.query("SELECT * FROM post ORDER BY created_at DESC")
+        res.json(rows)
+    }
+    catch (err) {
+        next(err)
+    }
 })
 
 router.get('/error', (req, res) => {
