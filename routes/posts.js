421bdc4a7f6b52eaaf699db0bbbbc9caa18cb2d9
RasmusEriksson <rasmus.eriksson@elev.ga.ntig.se>
2026-02-23 09:41:27 +0100

    provade på prepared statements

--- routes/posts.js
+++ routes/posts.js
@@ -21,6 +21,39 @@ router.get("/", async (req, res, next) => {
     }
 })
 
+router.get("/:id", async (req,res,next) => {
+    try {
+        const postId = req.params.id
+        if (!Number.isInteger(Number(postId))) {
+            throw new Error("Id is not a valid integer");
+            
+        }
+
+        const [row] = await pool.query(`
+            SELECT post.id, post.title, post.created_at, user.name
+            FROM post 
+            JOIN user ON post.user_id = user.id
+            WHERE post.id = ?
+            `,[postId]
+        )
+
+        if (row.length === 0) {
+            throw new Error("Post does not exist!")
+        }
+
+        res.render("posts.njk", {
+            title:"Blogposts!",
+            rows:row
+        })
+
+        console.log(row)
+        
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
 router.get('/error', (req, res) => {
     throw new Error('Test error')
 })
